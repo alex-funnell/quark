f9c1c4bb30724374c78220a6a32cec2cc4679b3c
Alex F <skimpilot@hotmail.com>
2020-02-17 20:22:25 +0000

    Updated prime.py

--- prime.py
+++ prime.py
@@ -2,16 +2,16 @@ from mpi4py import MPI
 import time
 import sys
 
-# Attach to the cluster and find out which node I am and how big cluster is
-comms = MPI.COMM_WORLD
-rank = comms.Get_rank()
-cluster_size = comms.Get_size()
+# Attach to the cluster and find out who I am and how big it is
+comm = MPI.COMM_WORLD
+my_rank = comm.Get_rank()
+cluster_size = comm.Get_size()
 
 # Number to start on, based on the node's rank
-start = int((rank * 2) + 1)
+start_number = (my_rank * 2) + 1
 
-# When to stop
-end = int(sys.argv[1])
+# When to stop. Play around with this value!
+end_number = int(sys.argv[1])
 
 # Make a note of the start time
 start = time.time()
@@ -20,7 +20,8 @@ start = time.time()
 primes = []
 
 # Loop through the numbers using rank number to divide the work
-for candidate_number in range(start, end, cluster_size * 2):
+for candidate_number in range(start_number,
+                              end_number, cluster_size * 2):
 
     # Log progress in steps
     # print(candidate_number)
@@ -37,19 +38,19 @@ for candidate_number in range(start, end, cluster_size * 2):
     # If we get here, nothing divided, so it's a prime number
     if found_prime:
         # Uncomment the next line to see the primes as they are found (slower)
-        print('Node ' + str(rank) + ' found ' + str(candidate_number))
+        # print('Node ' + str(my_rank) + ' found ' + str(candidate_number))
         primes.append(candidate_number)
 
-# Once complete, send results to the controller
-results = comms.gather(primes, root=0)
+# Once complete, send results to the governing node
+results = comm.gather(primes, root=0)
 
-# If node is controller, show results
-if rank == 0:
+# If I am the governing node, show the results
+if my_rank == 0:
 
-    # Duration
+    # How long did it take?
     end = round(time.time() - start, 2)
 
-    print('Find all primes up to: ' + str(end))
+    print('Find all primes up to: ' + str(end_number))
     print('Nodes: ' + str(cluster_size))
     print('Time elasped: ' + str(end) + ' seconds')
 
@@ -58,5 +59,4 @@ if rank == 0:
     merged_primes.sort()
     print('Primes discovered: ' + str(len(merged_primes)))
     # Uncomment the next line to see all the prime numbers
-    print('Primes found:')
-    print(merged_primes)
+    # print(merged_primes)
