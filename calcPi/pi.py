ec1321575dc311698638076d9d07a17af999b5c8
vulcno <vulcno@icloud.com>
2020-02-22 16:45:48 +0000

    Added pi.py

---
+++ calcPi/pi.py
@@ -0,0 +1,53 @@
+# This code runs on a single computer and DOES NOT SUPPORT PARALLEL COMPUTING
+# An explanation why this won't work in parallel is available in the README
+# This will calculate the digits of pi for 120 seconds.
+# When I tested it, I got around 38K digits.
+
+# If you change the duration to 10, then you should get around 10K digits.
+# This algorithm gets harder as it continues due to the nature of it. 
+# It has an exponentially increasing difficulty. Read more about it in the README!
+
+
+import multiprocessing
+import time
+import sys
+from tqdm import tqdm
+
+def calcPi():
+    q, r, t, k, n, l = 1, 0, 1, 1, 3, 3
+    while True:
+        if 4 * q + r - t < n * t:
+            yield n
+            nr = 10 * (r - n * t)
+            n  = ((10 * (3 * q + r)) // t) - 10 * n
+            q  *= 10
+            r  = nr
+        else:
+            nr = (2 * q + r) * l
+            nn = (q * (7 * k) + 2 + (r * l)) // (t * l)
+            q  *= k
+            t  *= l
+            l  += 2
+            k += 1
+            n  = nn
+            r  = nr
+
+i = 0
+pi_digits = calcPi()
+startTime = time.time()
+duration = 120
+endTime = startTime + duration
+validTime = True
+
+
+
+for digit in pi_digits:
+    sys.stdout.write(str(digit))
+    i += 1
+    currentTime = time.time()
+    if currentTime == endTime or currentTime > endTime and validTime:
+        print('Digits found:',str(i))
+        timeTaken = currentTime-startTime
+        print('Took',timeTaken,'seconds.')
+        validTime = False
+        exit()
